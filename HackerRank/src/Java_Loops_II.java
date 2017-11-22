0e316c28bfba1cd5b2331983534ba6ddc839d8f3
kumara2 <kumara2@DLHCWD0976.dlh.st.com>
2017-11-22 15:10:31 +0530

    First Commit

---
+++ HackerRank/src/Java_Loops_II.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Java_Loops_II {
+
+	public static void main(String[] args) {
+		 Scanner in = new Scanner(System.in);
+		int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int result = a;
+            for(int j=0; j<n; j++) {
+            	
+            	result += (2^j)*b;
+            	
+            	System.out.print(result+" ");
+            }
+            
+            /*IntStream.iterate(1, num -> num+1)
+            		 .*/
+        }
+        in.close();
+
+	}
+
+}
